7c8c2cbe10de7a9cd34b741b35c242a0f40b7569
Matthew Tanner <115193+matthew-tanner@users.noreply.github.com>
2021-07-04 10:24:31 -0500

    fixed setEvents issue

--- src/Components/TicketMaster.js
+++ src/Components/TicketMaster.js
@@ -1,49 +1,45 @@
-import React, {useState, useEffect} from 'react' 
-import Geohash from 'latlon-geohash'
-import TicketMasterDisplay from './TicketmasterDisplay'
+import React, { useState, useEffect } from "react";
+import Geohash from "latlon-geohash";
+import TicketMasterDisplay from "./TicketmasterDisplay";
 
-let TicketMaster = ({latitude, longitude}) => {
-    const [events, setEvents] = useState()
-    //I need to do some sort of logic incorporating the id of the event with the url.
-    //So may mapping (event => something )
+let TicketMaster = ({ latitude, longitude }) => {
+  const [events, setEvents] = useState([]);
+  //I need to do some sort of logic incorporating the id of the event with the url.
+  //So may mapping (event => something )
 
- // useEffect(() => {
-    //     const geo = Geohash.encode(latitude, longitude, 5)
-    //     fetch (`https://app.ticketmaster.com/discovery/v2/events.json?&geoPoint=${geo}&apikey=lW1m9JXhPGuTVFc9OgbCKeqsPMg9qGTB`)
-    //     .then(res => {
-    //         console.log(res.json())
-    //     .then(data => setLocalEvents(data._embedded.events))
-    //         // setLocalEvents(res._embedded)
-    //         })
-    //     .catch(err => console.log(err))
-    // })
+  // useEffect(() => {
+  //     const geo = Geohash.encode(latitude, longitude, 5)
+  //     fetch (`https://app.ticketmaster.com/discovery/v2/events.json?&geoPoint=${geo}&apikey=lW1m9JXhPGuTVFc9OgbCKeqsPMg9qGTB`)
+  //     .then(res => {
+  //         console.log(res.json())
+  //     .then(data => setLocalEvents(data._embedded.events))
+  //         // setLocalEvents(res._embedded)
+  //         })
+  //     .catch(err => console.log(err))
+  // })
 
-    const fetchTicketMaster = () => {
-        console.log(latitude, longitude);
-        const geo = Geohash.encode(latitude, longitude, 6)
-        console.log(geo);
-        fetch (`https://app.ticketmaster.com/discovery/v2/events.json?&geoPoint=${geo}&apikey=lW1m9JXhPGuTVFc9OgbCKeqsPMg9qGTB`)
-        .then(async res => {
-            try {
-                const data = await res.json()
-                setEvents(data._embedded.events[0].name)
-            } catch (err) {
-                console.error(err);
-            }
-        })
-    }
-    
+  const fetchTicketMaster = () => {
+    console.log(latitude, longitude);
+    const geo = Geohash.encode(latitude, longitude, 6);
+    console.log(geo);
+    fetch(
+      `https://app.ticketmaster.com/discovery/v2/events.json?&geoPoint=${geo}&apikey=lW1m9JXhPGuTVFc9OgbCKeqsPMg9qGTB`
+    ).then((res) => res.json())
+    .then(data => {
+      setEvents(data);
+      console.log(data._embedded.events);
+    })
+  };
 
-    return(
+  return (
     <>
-    <span>Search for Events in Your Area</span>
-    
-    <button onClick={fetchTicketMaster}>Search</button>
+      <span>Search for Events in Your Area</span>
 
-       <TicketMasterDisplay events={events} />
-       
+      <button onClick={fetchTicketMaster}>Search</button>
+
+      <TicketMasterDisplay events={events} />
     </>
-)
-}
+  );
+};
 
-export default TicketMaster
+export default TicketMaster;
--- src/Components/TicketmasterDisplay.js
+++ src/Components/TicketmasterDisplay.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const TicketMasterDisplay = ({events}) => {
  return(
-        <p>{events}</p>
+        <p>stuff</p>
  )
 
 
