12d0c45a3c3f06e2df60e76f6992f62cc34ed782
Janet Ziems <janetziems@Janets-MacBook-Air.local>
2021-07-04 19:09:11 -0400

    Added Material UI for button and card styling.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import {
     Switch
 } from 'react-router-dom'
 import Home from './Home'
-import EarthImage from './EarthImage'
+import GetEarthImage from './GetEarthImage'
 
 const Sidebar = ({ latitude, longitude }) => {
     console.log(latitude, longitude)
@@ -13,14 +13,14 @@ const Sidebar = ({ latitude, longitude }) => {
             <div className='sidebar-list-styling'>
                 <ul className='sidebar-list list-unstyled'>
                     <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/earthimage'>Earth Images</Link></li> 
+                    <li><Link to='/getearthimage'>Satellite Image</Link></li> 
                 </ul>
             </div>
 
             <div className='sidebar-route'>
                 <Switch>
                     <Route exact path='/home'><Home /></Route>
-                    <Route exact path='/earthimage'><EarthImage latitude={latitude} longitude={longitude} /></Route>
+                    <Route exact path='/getearthimage'><GetEarthImage latitude={latitude} longitude={longitude} /></Route>
                     <Route exact path='/'><Home /></Route>
                 </Switch>
             </div>
