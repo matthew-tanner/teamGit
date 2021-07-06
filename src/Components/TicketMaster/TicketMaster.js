7816c275a33fa34e49140f23e61d81eabdf9bad1
lhoward42 <lauricehoward42@gmail.com>
2021-07-05 22:58:58 -0400

    grabbing update of files

--- src/Components/TicketMaster/TicketMaster.js
+++ src/Components/TicketMaster/TicketMaster.js
@@ -1,39 +1,71 @@
-import React, { useState } from "react";
-import Geohash from "latlon-geohash";
-import Toolbar from "@material-ui/core/Toolbar";
-import TicketMasterDisplay from "./TicketmasterDisplay";
+import React, {useState, useEffect} from 'react' 
+import { makeStyles } from "@material-ui/core/styles";
+import { Toolbar } from "@material-ui/core";
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Geohash from 'latlon-geohash'
+import TicketMasterDisplay from './TicketmasterDisplay'
 
-let TicketMaster = ({ latitude, longitude }) => {
-  const [events, setEvents] = useState([]);
 
-  const fetchTicketMaster = () => {
-    console.log(latitude, longitude);
-    const geo = Geohash.encode(latitude, longitude, 5);
-    console.log(geo);
-    fetch(
-      `https://app.ticketmaster.com/discovery/v2/events.json?&geoPoint=${geo}&apikey=lW1m9JXhPGuTVFc9OgbCKeqsPMg9qGTB`
-    ).then(async (res) => {
-      try {
-        const json = await res.json();
-        const data = json._embedded.events;
-        console.log(data);
-        setEvents(data);
-      } catch (err) {
-        console.error(err);
-      }
-    });
-  };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    width: "100%"
+  },
+  palette: {
+    primary: {
+      light: "#f7fbfc",
+      main: "#d6e6f2",
+      dark: "#B9D7EA",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#f7fbfc",
+      main: "#d6e6f2",
+      dark: "#f85050",
+      contrastText: "#000",
+    },
+  },
+}));
 
-  return (
+let TicketMaster = ({latitude, longitude}) => {
+    const [events, setEvents] = useState([])
+    const classes = useStyles();
+
+    const fetchTicketMaster = () => {
+        console.log(latitude, longitude);
+        const geo = Geohash.encode(latitude, longitude, 4)
+        console.log(geo);
+        fetch (`https://app.ticketmaster.com/discovery/v2/events.json?&geoPoint=${geo}&apikey=lW1m9JXhPGuTVFc9OgbCKeqsPMg9qGTB`)
+        .then(async res => {
+            try {
+                const json = await res.json()
+                const data = json._embedded.events
+                console.log(data)
+                setEvents(data)
+            } catch (err) {
+                console.error(err);
+            }
+        })
+    }
+    
+    useEffect(() => {
+      fetchTicketMaster();
+    }, []);
+  
+    return(
     <>
-    <Toolbar />
-      <span>Search for Events in Your Area</span>
+    <span>Search for Events in Your Area</span>
+    
+    <ButtonBase onClick={fetchTicketMaster} style={{ display: "flex", height: 300 }}>Search</ButtonBase>
 
-      <button onClick={fetchTicketMaster}>Search</button>
+    <TicketMasterDisplay events={events} />
 
-      <TicketMasterDisplay events={events} />
     </>
-  );
-};
+)
+}
 
-export default TicketMaster;
+export default TicketMaster
--- src/Components/TicketMaster/TicketmasterDisplay.js
+++ src/Components/TicketMaster/TicketmasterDisplay.js
@@ -1,32 +1,108 @@
 import React from "react";
-// import styled from 'styled-components'
+import { DataGrid } from "@material-ui/data-grid";
+import { makeStyles } from "@material-ui/core/styles";
 
-// const TicketMaster = styled.img`
-// width: calc(100% - 20px);
-// height: calc(100% - 20px);
-// margin: 10px 10px 0 10px'
-// `
-const TicketMasterDisplay = ({ events }) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    width: "100%"
+  },
+  palette: {
+    primary: {
+      light: "#f7fbfc",
+      main: "#d6e6f2",
+      dark: "#B9D7EA",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#f7fbfc",
+      main: "#d6e6f2",
+      dark: "#f85050",
+      contrastText: "#000",
+    },
+  },
+}));
 
-  const rows = []
+const TicketMasterDisplay = (props) => {
+  const classes = useStyles();
+  const rows = [];
+  const columns = [
+    {
+      field: "col1",
+      headerName: "Events",
+      headerClassName: "",
+      headerAlign: "center",
+      width: 150,
+    },
+    {
+      field: "col2",
+      headerName: "Distance",
+      headerClassName: "",
+      headerAlign: "center",
+      width: 150,
+    },
+    {
+      field: "col3",
+      headerName: "Time",
+      headerClassName: "",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "col4",
+      headerName: "Date",
+      headerClassName: "",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "col5",
+      headerName: "Get Tickets",
+      headerClassName: "",
+      headerAlign: "center",
+      width: 150
+    },
+  ]
 
+  const table = () => {
+    return(
+    <main className={classes.content} style={{ display: "flex", height: 600 }}>
+      {eventMap()}
+      <DataGrid
+          autoPageSize
+          pagination
+          pageSize={10}
+          rows={rows}
+          columns={columns}
+          rowHeight={25}
+        />
+      </main>
+      )
+  }
   const eventMap = () => {
-    return events.map((event, index) => {
+    console.log(props.events);
+    props.events.map((event, index) => { 
       return rows.push({
         id: index,
         col1: event.name,
         col2: event.distance,
         col3: event.time,
         col4: event.dates.start.localDate,
-        col5: <a href={event.url}>Click Here for Tickets</a>,
-        col6: <img src={event.images[0].url} />,
+        col5: event.url
+        // col6: <img src={event.images[0].url} />,
       });
     });
   };
   return (
-    <>
+    <div>
+      {table()}
       {eventMap()}
-    </>
+      
+    </div>
   );
 };
 
